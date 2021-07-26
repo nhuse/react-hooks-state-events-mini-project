deda91cc509b7970d6ca75841b09a52a3f058b27
nhuse <nathan.huse@yahoo.com>
2021-07-26 10:15:07 -0500

    commit tree f893e73eef4a6c0a173ca8c9ae289a4be23ecd4e

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Task from "./Task";
+import { v4 as uuid} from "uuid";
 
-function TaskList() {
+function TaskList({ tasks }) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map(task => (
+        <Task key={uuid()} text={task.text} category={task.category} />
+      ))}
     </div>
   );
 }
